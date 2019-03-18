66f68b7189a24d456df240570050f6bcb532e0c2
yamsiu <mar6861214@gmail.com>
2019-03-18 08:10:46 +0800

    建立 Layout, BurgerBuilder component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
-
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurguerBuilder/BurgurBuilder';
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Aux from "../../hoc/Auxiliary";
+import classes from "./Layout.css";
+const layout = props => (
+  <Aux>
+    <div>Toolbar, SIdeDrawer, Backdrop</div>
+    <main className={classes.content}>{props.children}</main>
+  </Aux>
+);
+
+export default layout;
